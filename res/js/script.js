192c66b034fb3628e552bb6dc4f16834abd50c4f
jirkapriv <jiri.privratsky@student.spsmb.cz>
2023-03-22 18:31:41 +0100

    movement added

--- res/js/script.js
+++ res/js/script.js
@@ -1,16 +1,13 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
-let cX;
-let cY;
+let radius = 50;
+let speed = 1;
 
 let hue = Math.random() * 360;
 let colorCircle = "hsl(" + hue + ",100%,50%)";
 let borderColorCircle = "hsl(" + hue + ",100%,30%)";
-let borderThickness = 10;
-let circleX = 200;
-let circleY = 200;
-let circleRadius = 100;
+let borderThickness = 5;
 
 let resize = () => {
   canvas.width = window.innerWidth + 2000;
@@ -18,21 +15,51 @@ let resize = () => {
 };
 
 resize();
+let circleX = canvas.width / 2;
+let circleY = canvas.height / 2;
+let cX = circleX;
+let cY = circleY;
 
-ctx.beginPath();
-ctx.fillStyle = colorCircle;
-ctx.strokeStyle = borderColorCircle;
-ctx.lineWidth = borderThickness;
-ctx.arc(circleX, circleY, circleRadius, 0, 2 * Math.PI);
-ctx.stroke();
-ctx.fill();
+function draw() {
+  const delkaX = cX - circleX;
+  const delkaY = cY - circleY;
+  const distance = Math.sqrt(delkaX ** 2 + delkaY ** 2);
+  if (distance > speed) {
+    const zpomalovac = speed / distance;
+    circleX += delkaX * zpomalovac;
+    circleY += delkaY * zpomalovac;
+  } else {
+    circleX = cX;
+    circleY = cY;
+  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(circleX, circleY, radius, 0, Math.PI * 2);
+  ctx.fillStyle = colorCircle;
+  ctx.strokeStyle = borderColorCircle;
+  ctx.lineWidth = borderThickness;
+  ctx.fill();
+  ctx.stroke();
 
-let getCursorCoo = (event) => {
-  cX = event.clientX;
-  cY = event.clientY;
-  console.log(cX + " " + cY);
-};
+  requestAnimationFrame(draw);
+}
 
-document.addEventListener("mousemove", getCursorCoo);
+function start() {
+  resize();
+  window.addEventListener("resize", () => {
+    resize();
+    draw();
+  });
 
+  window.addEventListener("mousemove", (event) => {
+    const canvasRect = canvas.getBoundingClientRect();
+    cX = event.clientX - canvasRect.left;
+    cY = event.clientY - canvasRect.top;
+  });
 
+  requestAnimationFrame(draw);
+}
+
+start();
+
+console.log(canvas.getBoundingClientRect());
